3aeb0fe99e28e353e0cb69974d44714be79654e2
srittam-personal <srittam89@gmail.com>
2022-08-04 18:40:04 +0530

    commit 4

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js Srittam - feature 2!</a>
+          Welcome to <a href="https://nextjs.org">Next.js Srittam - feature 3!</a>
         </h1>
 
         <p className={styles.description}>
