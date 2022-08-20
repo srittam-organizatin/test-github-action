c9467469d16626f9c29eeebff89cb0767f7da1d3
srittam-personal <srittam89@gmail.com>
2022-08-20 20:00:31 +0530

    tet

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js Srittam - feature 5!</a>
+          Welcome to <a href="https://nextjs.org">Next.js Srittam - feature 522!</a>
         </h1>
 
         <p className={styles.description}>
